a3bce54ec828d137bce21f6099ae30636d35827f
FakeChanelini <blakespurdle@gmail.com>
2023-12-28 17:34:36 +1300

    body colour black

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ class Header extends Component {
     var dataThemeAttribute = 'data-theme'
     var body = document.body
     var newTheme =
-      body.getAttribute(dataThemeAttribute) === 'dark' ? 'light' : 'dark'
+      body.getAttribute(dataThemeAttribute) === 'dark' ? 'dark' : 'dark'
     body.setAttribute(dataThemeAttribute, newTheme)
   }
 
