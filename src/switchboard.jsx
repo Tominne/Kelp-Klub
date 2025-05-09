25b081c776cdbe1b06b4f7722f95ed28adfb9fd7
Min Iles <min.elliot48@gmail.com>
2025-05-09 14:07:50 +1200

    kelp forest image added

--- src/switchboard.jsx
+++ src/switchboard.jsx
@@ -1,19 +1,29 @@
-import React, {useState} from "react";
-import { useQuery } from 'react-query';
+import React, { useState } from 'react'
+import { useQuery } from 'react-query'
 
 import './App.scss'
 
 const fetchSwitchBoardData = async () => {
   return [
-  { name: 'Eat loads of carbon', state: false},
-  { name: 'Be a good home to the creatures even if they nibble on you', state: false},
-  { name: 'Green', state: false},
-  { name: 'If people think youre gross too bad', state: false}
+    {
+      name: 'Our Giant Kelp Forests, soaking up lost of carbon, fast growing, home and shelter to much of our ocean wildlife',
+      state: false,
+      image: 'Giant-Kelp.jpg',
+    },
+    {
+      name: 'Be a good home to the creatures even if they nibble on you',
+      state: false,
+    },
+    { name: 'Green', state: false },
+    { name: 'If people think youre gross too bad', state: false },
   ]
 }
 
 const Switchboard = () => {
-  const { data, isLoading, error } = useQuery('switchboardData', fetchSwitchBoardData)
+  const { data, isLoading, error } = useQuery(
+    'switchboardData',
+    fetchSwitchBoardData
+  )
   const [switches, setSwitches] = useState([])
 
   React.useEffect(() => {
@@ -22,18 +32,18 @@ const Switchboard = () => {
     }
   }, [data])
 
-
   if (isLoading) return 'Loading...'
   if (error) return 'Error has occured'
 
-  return(
+  return (
     <div className="switchboard">
       <h1 className="title">What we can learn from seaweed...</h1>
       <br></br>
       {switches.map((item, index) => (
         <div className="switchboard" key={index}>
           <h2 className="switchboard">{item.name}:</h2>
-         </div>
+          <img src={item.image} alt={item.name} className="switchboard-image" />
+        </div>
       ))}
     </div>
   )
